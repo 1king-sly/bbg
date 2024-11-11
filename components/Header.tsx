a56ff36bcda132db0706f73b74dee7034612105c
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-11-11 15:26:54 +0300

    updated the header to make it responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,10 +7,21 @@ import { ModeToggle } from "./mode-toggle"
 import { Heart } from "lucide-react"
 import { usePathname } from 'next/navigation'
 import { AuthModal } from "./AuthModal"
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
+import { Compass, Menu, Moon, Sun, User, X } from "lucide-react";
+
+
 
 const Header = () => {
   const pathname = usePathname()
   const [isAuthModalOpen, setIsAuthModalOpen] = useState(false)
+  const [session,setSession] =useState(false)
+
+  // const access_token = localStorage.getItem("accessToken");
+
+  // if(access_token !== null && access_token !== ""){
+  //   setSession(true)
+  // }
 
   const isLoggedIn = pathname.startsWith('/dashboard')
   let dashboardLink = '/dashboard'
@@ -26,16 +37,16 @@ const Header = () => {
   }
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
-        <div className="mr-4 hidden md:flex">
-        <Link href="/" className="mr-6 flex items-center space-x-2">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 mx-auto px-5">
+      <div className="container flex h-14 items-center  justify-between">
+        <div className="flex w-[80%] md:w-[70%] justify-between">
+        <Link href="/" className=" flex items-center space-x-2">
             <Heart className="h-6 w-6 text-primary"  />
-            <span className="hidden font-bold sm:inline-block text-primary">
+            <span className=" font-bold sinline-block text-primary">
               BabyGal
             </span>
           </Link>
-          <nav className="flex items-center space-x-6 text-sm font-medium">
+          <nav className="flex items-center space-x-6 text-sm font-medium hidden md:block">
             <Link href="/about">About</Link>
             <Link href="/services">Services</Link>
             <Link href="/events">Events</Link>
@@ -48,7 +59,37 @@ const Header = () => {
           <div className="w-full flex-1 md:w-auto md:flex-none">
            
           </div>
-          <nav className="flex items-center">
+
+          {/* Mobile Navigation */}
+      <Sheet>
+          <SheetTrigger asChild className="md:hidden">
+            <Button variant="ghost" size="icon">
+              <Menu className="h-6 w-6" />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <div className="flex flex-col h-full">
+              <nav className="flex flex-col h-full items-center justify-center w-full gap-3">
+              <Link href="/about">About</Link>
+            <Link href="/services">Services</Link>
+            <Link href="/events">Events</Link>
+            <Link href="/profiles/1">Girls</Link>
+            <Link href="/organizations">Organizations</Link>
+            <Link href="/contact">Contact</Link>
+            <Button variant="ghost" className="mr-2" onClick={() => setIsAuthModalOpen(true)}>
+                Login / Sign up
+              </Button>
+
+              <ModeToggle />
+
+              </nav>
+              <div className="flex flex-col space-y-4 mb-8">
+                
+              </div>
+            </div>
+          </SheetContent>
+        </Sheet>
+          <nav className="hidden md:flex items-center">
             {isLoggedIn ? (
               <>
                 <Link href={dashboardLink} passHref>
@@ -72,6 +113,8 @@ const Header = () => {
         </div>
       </div>
       <AuthModal isOpen={isAuthModalOpen} onClose={() => setIsAuthModalOpen(false)} />
+
+      
     </header>
   )
 }
