d557e6eb6eb5a171783ddf5044be22e4de2d2284
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-12-05 11:17:04 +0300

    users can see the events they rsvp'd for and their is an event status based on the current date

--- components/dashboard/user/UserEvents.tsx
+++ components/dashboard/user/UserEvents.tsx
@@ -65,7 +65,6 @@ export default function AdminEvents() {
       });
 
       const data = await response.json();
-      console.log(data)
       if (response.ok) {
         setEvents(data);
       }
