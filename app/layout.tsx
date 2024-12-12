ff67329c3ba30c57454121b87a8af500e280792b
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-12-12 14:32:52 +0300

    adsense build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
     <head>
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9941754648272115"
-     crossorigin="anonymous"></script>
+     crossOrigin="anonymous"></script>
     </head>
       <body className={inter.className}>
         <ThemeProvider
